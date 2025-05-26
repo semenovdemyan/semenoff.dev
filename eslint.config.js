aad70cb69de61a56ea8fddb15c6200d14ad37dda
Demian Semenov <semenovdemyan@gmail.com>
2025-05-26 23:22:49 +0300

    update

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      'coverage',
+      '.eslintrc.js',
+      '.babel.config.js',
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
